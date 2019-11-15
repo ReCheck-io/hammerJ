ca1885b50f5a562fdd328c8df5d1399108f59ea2
xxLestadxx <j.v.gospodinov@gmail.com>
2019-11-15 17:42:09 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-An encryption library using BLAKE2s and Session25519.
+# Encryption library used for blockchain workflow. 
 
-Created by Recheck BV. 
+# Resources
+
+## JBlake2
+- Origin: https://github.com/kocakosm/jblake2
+- License: **GNU Lesser General Public License**
+
+## Scrypt 
+- Origin: https://github.com/wg/scrypt
+- License: **Apache License 2.0**
+
+## TweetNaCl-java
+- Origin: https://github.com/InstantWebP2P/tweetnacl-java
+- License: **MIT Copyright(2014-2017) by tom zhou, iwebpp@gmail.com**
+
+## Web3J (mainly using keccak256)
+- Origin: https://github.com/web3j/web3j
+- License: **Apache License 2.0** 
+
+### Created by Recheck BV. 
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,32 +3,31 @@ import com.iwebpp.crypto.TweetNaclFast.Box;
 import com.iwebpp.crypto.TweetNaclFast.Signature;
 import com.lambdaworks.crypto.SCrypt;
 import org.kocakosm.jblake2.Blake2s;
+import org.web3j.crypto.Hash;
 
 import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 
 import static java.util.Arrays.copyOfRange;
 
+/*
+ Hash.sha3String(String a) is the equivalent to hash() / return '0x' + keccak256(src).toString('hex'); /
+  in hammerJS
+ */
 
 public class App {
     public static final String BOX_NONCE = "69696ee955b62b73cd62bda875fc73d68219e0036b7a0b37";
-    private static int logN = 131072;  // this number is 2^17  CPU/memory cost parameter (1 to 31)
-    private static int r = 8;    // block size parameter
-    private static int p = 1;   // Parallelization parameter.
-    private static int dkLen = 64;   // length of derived key in Bytes
 
     public static void main(String args[]) throws GeneralSecurityException {
 
-        byte[] key1 = "clod sg grata image nelsen gsa".getBytes();
-        Blake2s key1Bytes = new Blake2s(32); // A 32 Byte hash of the password
-        key1Bytes.update(key1);
-        byte[] blakeHash = key1Bytes.digest();
+        String key1 = "clod sg grata image nelsen gsa";
+        String key2 = "bode boxy 1992 deacon keep free";
 
-        byte[] key2 = "bode boxy 1992 deacon keep free".getBytes();
-
-        byte[] derivedBytes = SCrypt.scrypt(blakeHash, key2, logN, r, p, dkLen);
+        byte[] derivedBytes = session25519(key1, key2);
         System.out.println("Hex derived bytes " + bytesToHex(derivedBytes).length());
-        System.out.println("Hex encoding is "+ bytesToHex(derivedBytes).toLowerCase());
+        System.out.println("Hex encoding is " + bytesToHex(derivedBytes).toLowerCase());
         System.out.println("------------------------------");
+
         // Having the seed to the following two key pairs
         byte[] encryptKeySeed = copyOfRange(derivedBytes, 0, 32);
         byte[] signKeySeed = copyOfRange(derivedBytes, 32, 64);
@@ -47,6 +46,50 @@ public class App {
         System.out.println("Sign public user1 : " + Base58Check.encode(keyPairS.getPublicKey()));
         System.out.println("Sign secret user1: " + bytesToHex(keyPairS.getSecretKey()));
         System.out.println("------------------------------");
+
+    }
+
+    //non-static method cannot be referenced from a static context
+    private boolean sign(byte[] file, byte[] privateKey){
+        return Signature.detached_verify(file, privateKey);
+    }
+
+    private String hashString(String toHash) {
+        return Hash.sha3String(toHash);
+    }
+
+    private String encodeBase58(byte[] toEncode) throws NoSuchAlgorithmException {
+        return Base58Check.encode(toEncode);
+    }
+
+    private byte[] decodeBase58(String toDecode) throws NoSuchAlgorithmException {
+        return Base58Check.decode(toDecode);
+    }
+
+    // TODO: Whether this should stay an array or made into an object
+    // For now it will be a string array having all info ... should be converted into object
+    private static byte[] session25519(String key1, String key2) throws GeneralSecurityException {
+        int logN = 131072;  // this number is 2^17  CPU/memory cost parameter (1 to 31)
+        int r = 8;    // block size parameter
+        int p = 1;   // Parallelization parameter.
+        int dkLen = 64;   // length of derived key in Bytes
+
+        // takes the first six words and encodes them with Blake2s
+        byte[] key1Bytes = key1.getBytes();
+        Blake2s key1Blake = new Blake2s(32); // A 32 Byte hash of the password
+        key1Blake.update(key1Bytes);
+        byte[] blakeHash = key1Blake.digest();
+
+        // by definition, the second key is used as salt and need only be converted to byte[]
+        byte[] key2Bytes = key2.getBytes();
+
+        byte[] derivedBytes = SCrypt.scrypt(blakeHash, key2Bytes, logN, r, p, dkLen);
+
+        return derivedBytes;
+
+        // From the derivedBytes creating the seed for the following two key pairs
+//        byte[] encryptKeySeed = copyOfRange(derivedBytes, 0, 32);
+//        byte[] signKeySeed = copyOfRange(derivedBytes, 32, 64);
     }
 
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
